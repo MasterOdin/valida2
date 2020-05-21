8656e8e8a3fb8d0c6eb5616529287765a1990ca5
Matthew Peveler <matt.peveler@gmail.com>
2020-05-21 00:43:05 -0400

    add validators.required test
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- spec/validators.spec.js
+++ spec/validators.spec.js
@@ -1,1 +1,20 @@
 const validators = require('../lib/validators');
+const expect = require('chai').expect;
+
+describe('validators', () => {
+  describe('.required', () => {
+    it('undefined should return value', () => {
+      expect(validators.required(undefined)).to.eql({validator: 'required'});
+    });
+
+    it('null should return value', () => {
+      expect(validators.required(null)).to.eql({validator: 'required'});
+    });
+
+    [['', "''"], [0, '0'], [{}, '{}'], [[], '[]']].forEach(([value, repr]) => {
+      it(`should validate ${repr}`, () => {
+        expect(validators.required(value)).to.be.undefined;
+      });
+    });
+  });
+});
