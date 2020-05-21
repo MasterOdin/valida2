6164c53cc73928853cced4b0ec543b0a396fdf01
Matthew Peveler <matt.peveler@gmail.com>
2020-05-21 11:03:38 -0400

    change empty to notEmpty and add semantically correct empty
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- spec/validators.spec.js
+++ spec/validators.spec.js
@@ -11,9 +11,54 @@ describe('validators', () => {
       expect(validators.required(null)).to.eql({validator: 'required'});
     });
 
-    [['', "''"], [0, '0'], [{}, '{}'], [[], '[]']].forEach(([value, repr]) => {
-      it(`should validate ${repr}`, () => {
+    ['', 0, {}, []].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
         expect(validators.required(value)).to.be.undefined;
+      });
+    });
+  });
+
+  describe('.notEmpty', () => {
+    [undefined, null, 'aa', ['aa']].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.notEmpty(value)).to.be.undefined;
+      });
+    });
+
+    [[], ''].forEach((value) => {
+      it(`should not validate empty ${JSON.stringify(value)}`, () => {
+        expect(validators.notEmpty(value)).to.eql({validator: 'notEmpty'});
+      });
+    });
+  });
+
+  describe('.empty', () => {
+    [undefined, null, [], ''].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.empty(value)).to.be.undefined;
+      });
+    });
+
+    ['aa', ['aa']].forEach((value) => {
+      it(`should not validate non-empty ${JSON.stringify(value)}`, () => {
+        expect(validators.empty(value)).to.eql({validator: 'empty'});
+      });
+    });
+  });
+
+  describe('.float', () => {
+    [undefined, null, 123.423].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.float(value)).to.be.undefined;
+      });
+    });
+
+    [123, 123.0].forEach((value) => {
+      it(`should not validate ${JSON.stringify(value)}`, () => {
+        expect(validators.float(value)).to.eql({
+          validator: 'float',
+          msg: 'Invalid float value.',
+        });
       });
     });
   });
