1f051ad26a6a8637ea10f7a28658b12ee0a2e653
Matthew Peveler <matt.peveler@gmail.com>
2020-05-22 11:15:57 -0400

    add tests for len and array validators
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- spec/validators.spec.js
+++ spec/validators.spec.js
@@ -63,8 +63,69 @@ describe('validators', () => {
       });
     });
 
+    it('should coerce to string and then validate', () => {
+      expect(validators.regex(12345, {pattern: /[0-5]+/})).to.be.true;
+    });
+
     it('should not validate test for /tset/', () => {
       expect(validators.regex('test', {pattern: /tset/})).to.be.false;
+    });
+  });
+
+  describe('.len', () => {
+    [undefined, null].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.len(value, {})).to.be.true;
+      });
+    });
+
+    [
+      ['test', {}],
+      [['test'], {}],
+      [12345, {min: 1}],
+      ['test', {min: 1}],
+      ['test', {min: 4}],
+      ['test', {max: 5}],
+      ['test', {max: 4}],
+      [[1,2,3,4], {min: 1}],
+      ['test', {min: 1, max: 5}],
+      [[1,2,3,4], {min: 1, max: 5}],
+    ].forEach(([value, options]) => {
+      it(`should validate ${JSON.stringify(value)} with ${JSON.stringify(options)}`, () => {
+        expect(validators.len(value, options)).to.be.true;
+      });
+    });
+
+    [
+      ['test', {min: 5}],
+      ['test', {max: 3}],
+      [[1,2,3,4], {min: 5}],
+      ['test', {min: 1, max: 3}],
+      ['test', {min: 5, max: 6}],
+    ].forEach(([value, options]) => {
+      it(`should not validate ${JSON.stringify(value)} with ${JSON.stringify(options)}`, () => {
+        expect(validators.len(value, options)).to.be.false;
+      });
+    });
+  });
+
+  describe('.array', () => {
+    [undefined, null].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.array(value, {})).to.be.true;
+      });
+    });
+    [[], ['test']].forEach((value) => {
+      it(`should validate ${JSON.stringify(value)}`, () => {
+        expect(validators.array(value)).to.be.true;
+      });
+    });
+
+
+    ['test', 123, {}, true].forEach((value) => {
+      it(`should not validate ${JSON.stringify(value)}`, () => {
+        expect(validators.array(value)).to.be.false;
+      });
     });
   });
 
